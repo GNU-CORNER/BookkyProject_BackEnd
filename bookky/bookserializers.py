11533b185c1d9c11f79f1f2a2f47d4729c8dc6ab
황혁주 <nugulhie@gmail.com>
2022-03-12 23:21:55 +0900

    dev function about only save data #3

--- bookky/bookserializers.py
+++ bookky/bookserializers.py
@@ -5,29 +5,26 @@ from .models import Book
 class BookPostSerializer(serializers.ModelSerializer): #API에서 불러온 정보를 Book 데이터에 인젝션을 위해서 만든 serializer
     class Meta:
         model = Book
-        fields = ['TITLE', 'VOL', 'AUTHOR', 'EA_ISBN', 'EA_ADD_CODE', 'PUBLISHER', 'PRE_PRICE', 'PAGE', 'SUBJECT', 'BOOK_TB_CNT_URL','BOOK_INTRODUCTION_URL', 'INPUT_DATE', 'UPDATE_DATE']
+        fields = ['TITLE', 'VOL', 'AUTHOR', 'ISBN', 'PUBLISHER', 'PRICE', 'PAGE', 'SUBJECT', 'BOOK_INDEX','BOOK_INTRODUCTION','BOOK_SUMMARY' ,'INPUT_DATE']
 
 '''
-class Book(models.Model):
     BID                     = models.BigAutoField(primary_key=True)          #Primary Key
     TITLE                   = models.CharField(max_length=50, null=False)    #책 제목
     VOL                     = models.CharField(max_length=20, null=True)     #책 권차
     AUTHOR                  = models.CharField(max_length=20, null=False)    #저자
-    EA_ISBN                 = models.IntegerField(null=False)                #ISBN코드
-    EA_ADD_CODE             = models.IntegerField(null=False)                #ISBN 부가 기호
+    ISBN                    = models.CharField(max_length=20, null=False)    #ISBN코드             데이터 인젝트시에 String을 Integer로 바꿔서 넣어야함
     PUBLISHER               = models.CharField(max_length=20, null=False)    #발행처
-    PRE_PRICE               = models.IntegerField(null=False)                #가격
-    PAGE                    = models.CharField(max_length=20, null=False)    #페이지
+    PRICE                   = models.IntegerField(null=True)                 #가격                 데이터 인젝트시에 String을 Integer로 바꿔서 넣어야함
+    PAGE                    = models.CharField(max_length=20, null=True)     #페이지
     SUBJECT                 = models.CharField(max_length=20, null=True)     #주제(KDC대분류)
     thumbnail               = models.CharField(max_length=255, null=True)    #thumbnail API의 출력값 URL 변수명은 'TITLE_URL'
-    BOOK_TB_CNT_URL         = models.CharField(null=True)                    #목차
-    BOOK_INTRODUCTION_URL   = models.CharField(null=True)                    #책 소개
-    BOOK_SUMMARY_URL        = models.CharField(null=True)                    #책 요약
-    INPUT_DATE              = models.DateField(null=False)                   #등록날짜
-    UPDATE_DATE             = models.DateField(null=False)                   #수정날짜
-    Tag                     = models.CharField(max_length=100, null=False)   #태그
-    Rating                  = models.DoubleField(null=False, default = 2.5)  #별점 (기본값 2.5점)
+    BOOK_INDEX              = models.CharField(null=True)                    #목차
+    BOOK_INTRODUCTION       = models.CharField(null=True)                    #책 소개
+    BOOK_SUMMARY            = models.CharField(null=True)                    #책 요약
+    PUBLISH_DATE            = models.DateField(null=False)                   #등록날짜
+    UPDATE_DATE             = models.DateField(null=True)                    #수정날짜
+    TAG                     = models.CharField(max_length=100, null=False)   #태그
+    RATING                  = models.DoubleField(null=False, default = 2.5)  #별점 (기본값 2.5점)
     Allah_BID               = models.IntegerField(null=False)                #알라딘 고유 책번호
-
 
 '''
