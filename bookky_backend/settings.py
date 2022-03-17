7de75704e9de2097d8d33c0f672c4a2a68e36330
황혁주 <nugulhie@gmail.com>
2022-03-17 14:50:55 +0900

    24 setting security file for database, configuration

--- bookky_backend/settings.py
+++ bookky_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import .dbsetting
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,18 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%$u31ssin353atj40c1*fnxg!6x+*o-(ie)hl@vqpgs8quc@#^'
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bookkydb',
-        'USER' : 'hyeok',
-        'PASSWORD' : 'nicall1086',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
-    }
-}
-
+DATABASES = DATABASES
+SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
