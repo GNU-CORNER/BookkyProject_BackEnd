8dbe365bad869fb8b530184b02ef23eed9bbc735
황혁주 <nugulhie@gmail.com>
2022-03-18 16:03:15 +0900

    dev function bookAPI, add authorization function

--- bookky/auth.py
+++ bookky/auth.py
@@ -1,6 +1,10 @@
 import jwt
 import bcrypt
+
+from django.http import JsonResponse
+from rest_framework import status
 from bookky_backend import dbsetting
+from .models import User
 
 def setToken(rawData): # 비밀번호 토큰 생성
     hashed_pw = bcrypt.hashpw(rawData.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
@@ -16,3 +20,13 @@ def get_access(user): #ACCESS_TOKEN 발급
     secretKey = dbsetting.SECRET_KEY
     access_token = jwt.encode({'UID':user.UID}, secretKey, algorithm=dbsetting.algorithm)
     return access_token
+
+def valid_token(token):
+    try:
+        user = jwt.decode(token, dbsetting, algorithm=dbsetting.algorithm)
+        if(len(User.objects.filter(UID = user['UID'])) != 0):
+            return True
+        else :
+            return False
+    except jwt.exceptions.DecodeError:
+        return JsonResponse({'success':False, 'result': {}, 'errorMessage':"잘못된 토큰"}, status=status.HTTP_400_NOT_FOUND) 
--- bookky/bookviews.py
+++ bookky/bookviews.py
@@ -5,6 +5,7 @@ from django.http.response import JsonResponse
 from rest_framework.decorators import api_view
 from .models import Book
 from .bookserializers import BookPostSerializer
+from .auth import valid_token
 
 import requests
 import datetime
@@ -13,4 +14,28 @@ import json
 import time
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
-def book(request):   
+def book(request, slug):
+    if request.headers.get('Authorization')is None:
+        return JsonResponse({'success':False, 'result': {}, 'errorMessage':"Authroization키가 없음"}, status=status.HTTP_400_NOT_FOUND)
+    else:
+        tempToken = request.headers.get('Authorization')
+        if not valid_token(tempToken):
+            return JsonResponse({'success':False, 'result': {}, 'errorMessage':"유저 정보가 없음"}, status=status.HTTP_400_NOT_FOUND)
+        else :
+            try:
+                bookData = Book.objects
+            except Book.DoesNotExist:
+                return JsonResponse({'success':False, 'result': {}, 'errorMessage':"Book에 대한 데이터베이스가 존재하지 않거나, DB와의 연결이 끊어짐"}, status=status.HTTP_404_NOT_FOUND)
+        
+            if (request.method == 'GET'):
+                if slug ==  "0" :
+                    serializer = BookPostSerializer(bookData.all(), many=True)
+                    return JsonResponse({'success':True,'result' : serializer.data, 'errorMessage':""}, status=status.HTTP_200_OK)
+                else :
+                    filtered_data = bookData.filter(BID = slug)
+                    if len(filtered_data) == 0:
+                        return JsonResponse({'success':False, 'result':{}, 'errorMessage':"입력한 BID와 일치하는 정보가 없습니다."}, status=status.HTTP_204_NO_CONTENT)
+                    else:
+                        serializer = BookPostSerializer(filtered_data, many = True)
+                        return JsonResponse({'success':True, 'result' : serializer.data[0]}, status = status.HTTP_200_OK)
+    
